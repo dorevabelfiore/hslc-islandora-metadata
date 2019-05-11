745c171a876452f8899b19844c8955fa1b4de4de
Doreva Belfiore <dorevabelfiore@gmail.com>
2019-05-11 19:58:36 -0400

    Add files via upload
    Added versions of Stephen's files using Saxonica on Linux for HSLC Shell Server. Added version of postprocess-to-books for papd and psa namespapces on ingest-digitalarchives.powerlibrary.org. Added new postprocess-to-newspapers.pl and postprocess-to-newspapers-linux.pl for newspaper post-processing, per Stephen's intermediate script for newspapers.

---
+++ mods-processes/postprocess-to-books-select-linux.pl
@@ -0,0 +1,24 @@
+#! /usr/bin/perl -w
+use strict;
+
+system("time /t"); # time begin
+
+my $inputdir = "in";
+my $outputdir = "out";
+
+my @files = glob("$inputdir/*.xml");
+
+
+foreach my $infile (@files)
+{
+    my $outfile = $infile;
+    $outfile =~ s/^$inputdir/$outputdir/o;
+	
+	system("java -Xmx1000M -Xms1000M -cp /usr/share/java/saxon.jar net.sf.saxon.Transform -t -o:_tmp.1 -s:$infile -xsl:hslc-mods-updates-template.xsl");
+	system("java -Xmx1000M -Xms1000M -cp /usr/share/java/saxon.jar net.sf.saxon.Transform -t -o:$outfile -s:_tmp.1 -xsl:cdm-mods-final-level-to-books-select.xsl islandora-namespace=islandora");
+
+}
+
+unlink("_tmp.*");
+
+system("time /t"); # time end
